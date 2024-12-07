7ca854aa25bc7fc33509a59bad82fdc96beaee1e
Franck KN <fkinaninkaya@gmail.com>
2024-12-07 20:42:24 +0100

    Controllers and Views Update
    Add Controllers and Views Contribution and Project Improvements

---
+++ Controllers/ContributionController.cs
@@ -0,0 +1,100 @@
+﻿using CrowdFundingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrowdFundingApp.Controllers
+{
+    public class ContributionController : Controller
+    {
+        private CrowdFundingDbContext _context;
+
+        public ContributionController(CrowdFundingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ContributionController
+        public ActionResult Index()
+        {
+            var contributions = _context.Contributions.Include(c => c.User).Include(c => c.Project).ToList();
+            return View(contributions);
+        }
+
+        // GET: ContributionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ContributionController/Create
+        public ActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Username");
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "Title");
+            return View();
+        }
+
+        // POST: ContributionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("ContributionId,Amount,ContributionDate,UserId,ProjectId")] Contribution contribution)
+        {
+            try
+            {
+                _context.Contributions.Add(contribution);
+                ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Username", contribution.UserId);
+                ViewData["ProjectId"] = new SelectList(_context.Projects, "ProjectId", "Title", contribution.ProjectId);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContributionController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ContributionController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ContributionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ContributionController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
