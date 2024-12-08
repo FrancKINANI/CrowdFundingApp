ab3d85fe498f7077975f58fc332202aa767039fb
Franck KN <fkinaninkaya@gmail.com>
2024-12-08 12:35:30 +0100

    Admin and Simple User
    Start the configuration of Admin and Standard User Roles

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 using CrowdFundingApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Identity.Core;
 
-internal class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CrowdFundingDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("CrowdFundingConnection")));
+
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<CrowdFundingDbContext>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllersWithViews();
-        builder.Services.AddDbContext<CrowdFundingDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("CrowdFundingConnection")));
-
-        var app = builder.Build();
-
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
